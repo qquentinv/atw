99e35a28273ab16c91b49dd014ae0714d1b54dbe
qquentinv <quentinvoisin00@gmail.com>
2024-02-17 00:43:18 +0100

    feat: set zoom and first marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,37 @@
-import { MapContainer, TileLayer, Marker,Popup, useMap} from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const ComponentResize = () => {
-	const map = useMap();
+  const map = useMap();
 
-	setTimeout(() => {
-		map.invalidateSize();
-	}, 0);
+  setTimeout(() => {
+    map.invalidateSize();
+  }, 0);
 
-	return null;
+  return null;
 };
 
-
 function App() {
-const position = [36.0339, 1.6596];
-	
   return (
-			<MapContainer
-			style={{
-				height: '98vh'
-			}}
-			center={position}
-			attributionControl={true}
-			zoom={8}
-			minZoom={3}
-			scrollWheelZoom={true}
-		>
-			<ComponentResize />
-			<TileLayer
-				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			<Marker position={position}>
-				<Popup>
-					A pretty CSS3 popup. <br /> Easily customizable.
-				</Popup>
-			</Marker>
-		</MapContainer>
-  )
+    <MapContainer
+      style={{ height: '98vh' }}
+      center={[46.9908, 3.1628]}
+      attributionControl={true}
+      zoom={6}
+      scrollWheelZoom={true}
+    >
+      <ComponentResize />
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[49.4412,1.0499]}>
+        <Popup>
+					Rouen
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
-export default App
+export default App;
