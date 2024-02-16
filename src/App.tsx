ea72fe969ae31f4147d0bee5e48e0f766f86e912
qquentinv <quentinvoisin00@gmail.com>
2024-02-17 00:33:48 +0100

    feat: display map on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-function App() {
+import { MapContainer, TileLayer, Marker,Popup, useMap} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 
+const ComponentResize = () => {
+	const map = useMap();
+
+	setTimeout(() => {
+		map.invalidateSize();
+	}, 0);
+
+	return null;
+};
+
+
+function App() {
+const position = [36.0339, 1.6596];
+	
   return (
-    <>
-			<h1 style={{textAlign : 'center'}}>Hello world !</h1>
-		</>
+			<MapContainer
+			style={{
+				height: '98vh'
+			}}
+			center={position}
+			attributionControl={true}
+			zoom={8}
+			minZoom={3}
+			scrollWheelZoom={true}
+		>
+			<ComponentResize />
+			<TileLayer
+				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={position}>
+				<Popup>
+					A pretty CSS3 popup. <br /> Easily customizable.
+				</Popup>
+			</Marker>
+		</MapContainer>
   )
 }
 
