acbee44e39cf9e5b6163be8823aa6ed2dddcd5eb
qquentinv <quentinvoisin00@gmail.com>
2024-02-17 11:10:48 +0100

    feat: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
 })
